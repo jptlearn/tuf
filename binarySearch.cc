5c2ea4bcdced659192d680f6b9f9b2c5cbc54221
jptlearn <jptcode500@gmail.com>
2025-02-08 12:44:27 +0545

    solve binary search problem

---
+++ binarySearch.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int binarySearch(int arr[], int n, int target) {
+  int low = 0;
+  int high = n - 1;
+  while(low <= high) {
+    int mid = (low + high) / 2;
+    if(arr[mid] == target)
+      return mid;
+    else if(target > arr[mid])
+      low = mid + 1;
+    else
+      high = mid - 1;
+  }
+  return -1;
+}
+
+int binaryRecursiveSearch(int arr[], int low, int high, int target) {
+  if (low > high) {
+    return -1;
+  }
+  int mid = (low + high) / 2;
+  if(arr[mid] == target)
+    return mid;
+  else if(target > arr[mid])
+    return binaryRecursiveSearch(arr, mid + 1, high, target);
+  else
+    return binaryRecursiveSearch(arr, low, mid - 1, target);
+}
+
+int main() {
+  int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
+  int n = sizeof(arr) / sizeof(arr[0]);
+  int target = 3;
+  // int targetIndex = binarySearch(arr, 8, target);
+  int targetIndex = binaryRecursiveSearch(arr, 0, n - 1, target);
+  if (targetIndex == -1)
+  {
+    cout << "the target is not in the array" << endl;
+  }
+  else
+  {
+    cout << "the target is present in the array" << endl;
+  }
+
+  return 0;
+}
