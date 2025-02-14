dcaefa1c10aaa1117c6a2ae4f8c72c48326a533a
jptlearn <jptcode500@gmail.com>
2025-02-14 13:08:57 +0545

    update and check binarySearch algo

--- binarySearch.cc
+++ binarySearch.cc
@@ -30,6 +30,32 @@ int binaryRecursiveSearch(int arr[], int low, int high, int target) {
     return binaryRecursiveSearch(arr, low, mid - 1, target);
 }
 
+int binarySearch(int arr[], int low, int high, int target) {
+  while(low <= high){
+    int mid = (low + high) / 2;
+    if (arr[mid] == target)
+      return mid;
+    if(target < arr[mid])
+      high = mid - 1;
+    else
+      low = mid + 1;
+  }
+  return -1;
+}
+
+int binarySearchRecur(int arr[], int low,int high,int target) {
+  if(low > high)
+    return -1;
+  int mid = (low + high) / 2;
+  if(arr[mid] == target) {
+    return mid;
+  } else if (target < arr[mid]) {
+    return binarySearchRecur(arr, low, mid - 1, target);
+  } else {
+    return binarySearchRecur(arr, mid + 1, high, target);
+  }
+}
+
 int main() {
   int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
   int n = sizeof(arr) / sizeof(arr[0]);
