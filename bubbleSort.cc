322814011e7b3b65cb241e9076690bf8a6a2afcc
jptlearn <jptcode500@gmail.com>
2025-02-16 12:02:24 +0545

    modify bubbleSort file

--- bubbleSort.cc
+++ bubbleSort.cc
@@ -36,6 +36,24 @@ void bubbleSort(int arr[], int n) {
   }
 }
 
+void bubble(int arr[], int n) {
+  for (int i = n - 1; i >= 1; i--) {
+    int didSwap = 0;
+    for (int j = 0; j <= i - 1; j++)
+    {
+      if(arr[j] > arr[j+1]) {
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        didSwap = 1;
+      }
+    }
+    if(didSwap == 0) {
+      break;
+    }
+    cout << "runs\n";
+  }
+}
 // void recursive_bubble_sort(int arr[], int n) {
 //   if (n==1)
 //     return;
