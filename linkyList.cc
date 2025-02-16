d73d2661d4282181ee658813a9f6e7b3c4363c91
jptlearn <jptcode500@gmail.com>
2025-02-16 12:03:07 +0545

    solve link list related and modify code

--- linkyList.cc
+++ linkyList.cc
@@ -9,10 +9,8 @@ class Node {
     Node *next;
 
   public:
-    Node(int data1, Node* next1) {
-      data = data1;
-      next = next1;
-    }
+    Node(int data1, Node *next1) : data(data1), next(next1) {};
+
   public:
     Node(int data1) {
       data = data1;
